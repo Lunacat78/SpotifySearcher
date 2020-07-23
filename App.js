258b94e9f6e7171d10f0b51b14f33f62f78d213f
Nick Weber <NicholasMichaelWeber@gmail.com>
2020-07-23 19:56:48 -0400

    add follower count to alert when clicking artist

--- App.js
+++ App.js
@@ -1,34 +1,50 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, TextInput, FlatList, Image, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TextInput, FlatList, Image } from 'react-native';
 import { encode as btoa } from 'base-64';
 import credentials from './credentials.json';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 export default class App extends Component {
+  
+  // Our state data includes our current search bar
+  // and the data retrieved from Spotify
   state = {
     inputValue: '',
     data:[
     ],
   };
 
+  // Update our list when the search bar changes
   _handleTextChange = inputValue => {
     this.getArtistList(inputValue);
-    this.setState({ inputValue });
+    this.setState({inputValue: inputValue});
   };
   
   render() {
     return (
-      <View style={styles.container}>
-        <TextInput
-          value={this.state.inputValue}
-          onChangeText={this._handleTextChange}
-          style={{ width: 200, height: 44, padding: 8, borderWidth: 1, borderColor: '#ccc' }}
-        />
-        <FlatList
-          data={this.state.data}
-          renderItem={({ item }) => <Item item={item}/>}
-          keyExtractor={item => item.item}
-          extraData={this.state}
-        />
+      <View>
+        <View style={{alignSelf:"center"}}>
+          <TextInput
+            value={this.state.inputValue}
+            onChangeText={this._handleTextChange}
+            style={{ width: 200, height: 40, padding: 2, borderWidth: 1, borderColor: '#ccc' }}
+          />
+        </View>
+        <View style={styles.container}>
+          <FlatList
+            data={this.state.data}
+            renderItem={({ item }) => (
+              <TouchableOpacity onPress={ () => alert(item.name + " with " + item.followers + " Followers")}>
+                <View style={styles.listItem}>
+                  <Image source={{uri:item.image}}  style={{width:60, height:60,borderRadius:30}} />
+                  <Text style={styles.title}>{item.name}</Text>
+                </View>
+              </TouchableOpacity>
+            )}
+            keyExtractor={item => item.item}
+            extraData={this.state}
+          />
+        </View>
       </View>
     );
   }
@@ -49,6 +65,7 @@ export default class App extends Component {
     return { clientId, clientSecret };
   }
   
+  // Retrieve our OAuth token from Spotify with a POST request
   getToken = async () => {
     try {
       const credentials = await this.getCredentials()
@@ -62,6 +79,7 @@ export default class App extends Component {
         body: 'grant_type=client_credentials',
       });
   
+      // We'll calculate expiration time and token type, but we only need to pass the access token
       const responseJson = await response.json();
       const {
         access_token,
@@ -76,6 +94,7 @@ export default class App extends Component {
     }
   }
   
+  // Retrieve our search term's data with a GET request to Spotify
   getArtistList = async (searchTerm) => {
     try {
       const accessToken = await this.getToken();
@@ -87,10 +106,12 @@ export default class App extends Component {
       }).then(response => response.json())
       .then(response => {
         this.state.data = [];
+        // Get top 20 results for our search term and add to the state data
         for (var i = 0; i < 20; i++) {
-          console.log(response.artists.items[i].name);
-          this.state.data.push({name: response.artists.items[i].name, image: response.artists.items[i].images[0].url, followers: response.artists.items[i].popularity});
+          this.state.data.push({name: response.artists.items[i].name, image: response.artists.items[i].images[0].url, followers: response.artists.items[i].followers.total});
         }
+        // Force a state update to update the FlatList
+        this.setState(this.state);
       }).catch((error) => {
         console.log(error);
       });
@@ -101,27 +122,15 @@ export default class App extends Component {
 }
 
 
-function Item({ item }) {
-  return (
-    <View style={styles.listItem}>
-      <Image source={{uri:item.image}}  style={{width:60, height:60,borderRadius:30}} />
-      <Text style={styles.title}>{item.name}</Text>
-    </View>
-  );
-}
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F7F7F7',
-    marginTop:60
+    marginTop:10
   },
   listItem:{
     margin:10,
     padding:10,
-    backgroundColor:"#FFF",
-    width:"80%",
-    flex:1,
+    width:"90%",
     alignSelf:"center",
     flexDirection:"row",
     borderRadius:5
